2857c69b86dbee197413dfb139dc1fe53035a9e2
Guilherme Miranda <saguilherme16@gmail.com>
2025-08-07 18:56:46 -0300

    primeiro commit

---
+++ src/main/java/com/guilherme/workshopmongo/WorkshopSpringbootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopSpringbootMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guilherme/workshopmongo/WorkshopSpringbootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
