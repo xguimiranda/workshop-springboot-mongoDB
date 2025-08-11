a962d18d81961641d0e45feab2ff944bc699aa68
Guilherme Miranda <saguilherme16@gmail.com>
2025-08-11 19:56:40 -0300

    FEAT: Adicionando suporte a comentários nos posts

--- src/main/java/com/guilherme/workshopmongo/domain/Post.java
+++ src/main/java/com/guilherme/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.guilherme.workshopmongo.domain;
 
 import com.guilherme.workshopmongo.dto.AuthorDTO;
+import com.guilherme.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,8 @@ public class Post implements Serializable {
         private String title;
         private String body;
         private AuthorDTO author;
+
+        private List<CommentDTO> comments = new ArrayList<>();
 
         public Post() {
         }
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 
         public void setAuthor(AuthorDTO author) {
             this.author = author;
+        }
+
+        public List<CommentDTO> getComments() {
+            return comments;
+        }
+
+        public void setComments(List<CommentDTO> comments) {
+            this.comments = comments;
         }
 
         @Override
