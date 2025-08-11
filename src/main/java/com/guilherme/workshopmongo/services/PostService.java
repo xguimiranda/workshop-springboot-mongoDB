525964d0341b80f67fac1bf8ac379141c13f1d1c
Guilherme Miranda <saguilherme16@gmail.com>
2025-08-11 20:00:08 -0300

    FEAT: Implementar busca de posts por título

--- src/main/java/com/guilherme/workshopmongo/services/PostService.java
+++ src/main/java/com/guilherme/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.guilherme.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.guilherme.workshopmongo.domain.Post;
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
