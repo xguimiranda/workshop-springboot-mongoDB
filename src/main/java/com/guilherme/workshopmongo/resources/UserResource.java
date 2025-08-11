c60b47b79ec9bc9c663abf8110d1b61f45276e93
Guilherme Miranda <saguilherme16@gmail.com>
2025-08-11 18:46:20 -0300

    FEAT: integrate UserService into UserResource for user retrieval

--- src/main/java/com/guilherme/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guilherme/workshopmongo/resources/UserResource.java
@@ -1,25 +1,25 @@
 package com.guilherme.workshopmongo.resources;
 
 import com.guilherme.workshopmongo.domain.User;
+import com.guilherme.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("1", "Alex Green", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
